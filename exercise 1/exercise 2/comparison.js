9cd5fac79305a97bd8bb17cc96fce1248dc3ac6a
cloud <suleeqaabdi0@gmail.com>
2026-02-05 18:54:30 +0000

    submited exercise 2 & exercise 3

---
+++ exercise 1/exercise 2/comparison.js
@@ -0,0 +1,13 @@
+let x = 15;
+let y = "15";
+
+console.log(x == y)
+console.log(x != y)
+console.log(x === y)
+console.log(x !== y)
+
+console.log(x > y)
+console.log(x < y)
+console.log(x >= y)
+console.log(x <= y)
+
---
+++ exercise 1/exercise 3/logic.js
@@ -0,0 +1,7 @@
+let isSunny = true;
+let isWeekend = false;
+
+console.log(isSunny && isWeekend);
+console.log(isSunny || isWeekend);
+console.log(!isSunny);
+console.log(!isWeekend);
