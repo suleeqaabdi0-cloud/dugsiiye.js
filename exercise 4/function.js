470916f0df219df27ebd5a7e806ede1ea587dd98
cloud <suleeqaabdi0@gmail.com>
2026-02-09 18:08:02 +0000

    exercise 4 & exercise 5

---
+++ exercise 4/function.js
@@ -0,0 +1,16 @@
+function add(a , b){
+console.log(a+b) 
+}
+add(32,12);
+add(54,23);
+add(54,70);
+add(83,23);
+
+// function expresion
+const iskudar = function(a,b){
+    console.log(a+b)
+};
+iskudar(23,64)
+iskudar(23,78)
+iskudar(93,64)
+iskudar(45,64)
---
+++ exercise 5/array.js
@@ -0,0 +1,7 @@
+const add= (a,b)=>{
+    console.log(a+b)
+}
+add(23,67)
+add(54,67)
+add(23,21)
+add(78,98)
